d5bb5c33520131b538f11c5eaaa4f6e582912336
André Oliveira <andrelitoscria@gmail.com>
2025-10-13 19:26:12 -0300

    .

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';     // <-- Correção
+import { UsersModule } from './users/users.module';   // <-- Correção
+import { PrismaModule } from './prisma/prisma.module'; // <-- Correção
 
 @Module({
   imports: [AuthModule, PrismaModule, UsersModule],
--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { AuthModule } from 'src/auth/auth.module';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module'; // <-- Correção
 
 @Module({
   imports: [AuthModule, PrismaModule],
